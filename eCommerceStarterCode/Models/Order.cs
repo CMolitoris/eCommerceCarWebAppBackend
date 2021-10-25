92e9364381854a82105637168861a7448c44bf06
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-10-25 16:42:22 -0400

    Order class created

--- eCommerceStarterCode/Models/Order.cs
+++ eCommerceStarterCode/Models/Order.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerceStarterCode.Models
 {
     public class Order
     {
+        [Key]
+        public int Id { get; set; }
+        public double Total { get; set; }
+        public double ShippingCost { get; set; }
+        public bool Shipped { get; set; }
+        public int ShippingNumber { get; set; }
+        public bool Delivered { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
     }
+
+    
+
+    
 }
