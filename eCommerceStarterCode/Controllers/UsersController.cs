2fb295387ada8a1a57c862bf7b6fc34fcf7cad4d
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-25 17:52:15 -0400

    Created controller to Get All Users - Requires Testing

---
+++ eCommerceStarterCode/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Left out Authorize
+        [HttpGet("user")]
+        public IActionResult GetAllUsers()
+        {
+            var users = _context.Users;
+            return Ok(users);
+        }
+    }
+}
