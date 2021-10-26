a995115ded65ca1db493dd7e0c9e31b7eecf85cf
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-26 10:06:59 -0400

    Added cars to seed the cars table

--- eCommerceStarterCode/Data/ApplicationDbContext.cs
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -23,7 +23,9 @@ namespace eCommerceStarterCode.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Car>()
                 .HasData(
-                new Car { Id = 1, Price = 24000, Make = "Ford", Model = "Fusion", Description = "Entry level sedan", Mileage = 25000, AverageRating = 3.5 }
+                new Car { Id = 1, Price = 24000, Make = "Ford", Model = "Fusion", Description = "Entry level sedan", Mileage = 25000, AverageRating = 3.5 },
+                new Car { Id = 2, Price = 20000, Make = "Dodge", Model = "Durango", Description = "Used 2015 Dodge Durango", Mileage = 95000, AverageRating = 3.7 },
+                new Car { Id = 3, Price = 45000, Make = "Chevrolet", Model = "Tahoe", Description = "Used Chevy Tahoe SUV", Mileage = 40000, AverageRating = 4.2 }
                 );
 
             modelBuilder.ApplyConfiguration(new RolesConfiguration());
